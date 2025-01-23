46ae87d5fce78b924b4641b93ae54140178e6d26
Bagus Suprapta <bagussuprapta@gmail.com>
2025-01-23 11:04:05 +0800

    chore: install and setup cors

--- lingua-deck/server/src/apps/web.js
+++ lingua-deck/server/src/apps/web.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { publicRouter } from "../routes/publicRoute.js";
 import { errorMiddleware } from "../middlewares/errorMiddleware.js";
 import { privateRouter } from "../routes/privateRoute.js";
 
 export const web = express();
+web.use(cors());
 web.use(express.json());
 web.use(publicRouter);
 web.use(privateRouter);
