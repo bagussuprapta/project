c096fc03ae40a02b9777b84786c953a4f74098fc
Bagus Suprapta <bagussuprapta@gmail.com>
2025-01-28 18:02:48 +0800

    fix: update username validation to allow only letters

--- lingua-deck/server/src/validations/userValidation.js
+++ lingua-deck/server/src/validations/userValidation.js
@@ -1,14 +1,28 @@
 import Joi from "joi";
 
 const registerUserValidation = Joi.object({
-  username: Joi.string().min(3).max(50).required(),
+  username: Joi.string()
+    .min(3)
+    .max(15)
+    .required()
+    .pattern(/^[A-Za-z]+$/)
+    .messages({
+      "string.pattern.base": "username must contain only letters.",
+    }),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required().messages(),
   preferred_language: Joi.string().required().max(2),
 });
 
 const loginUserValidation = Joi.object({
-  username: Joi.string().min(3).max(50).required(),
+  username: Joi.string()
+    .min(3)
+    .max(15)
+    .required()
+    .pattern(/^[A-Za-z]+$/)
+    .messages({
+      "string.pattern.base": "username must contain only letters.",
+    }),
   password: Joi.string().min(6).required().messages(),
 });
 
